c8c510e87bef702852df1b544b073c8cf7320152
waizbart <guilhermewaizbart@gmail.com>
2024-10-03 14:14:48 -0300

    feat: error traits on controller

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -8,9 +8,11 @@ import {
   HttpException,
   HttpStatus,
   Logger,
+  Res,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from './guards/auth.guard';
+import { Response } from 'express';
 
 @UseGuards(AuthGuard)
 @Controller('api')
@@ -22,27 +24,51 @@ export class AppController {
   @Post('query')
   async generate(@Body('prompt') prompt: string): Promise<{ taskId: string }> {
     this.logger.log('Recebida requisição para gerar tarefa.');
+
     if (!prompt) {
       throw new HttpException('Prompt is required', HttpStatus.BAD_REQUEST);
     }
+
     const taskId = await this.appService.enqueueTask(prompt);
     return { taskId };
   }
 
   @Get('status/:taskId')
-  async getResult(@Param('taskId') taskId: string): Promise<any> {
+  async getResult(@Param('taskId') taskId: string, @Res() res: Response): Promise<any> {
     this.logger.log(`Consultando status da tarefa: ${taskId}`);
+
     const result = await this.appService.getTaskResult(taskId);
+
     if (!result || !result.status) {
-      throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
-    } else if (result.status === 'pending' || result.status === 'processing') {
-      return { status: result.status };
-    } else if (result.status === 'completed') {
-      return { status: 'completed', result: result.queryResult, generatedSql: result.generatedSQL };
-    } else if (result.status === 'error') {
-      return { status: 'error', error: result.error };
-    } else {
-      throw new HttpException('Unknown task status', HttpStatus.INTERNAL_SERVER_ERROR);
+      return res.status(HttpStatus.NOT_FOUND).json({ message: 'Task not found' });
+    }
+
+    const response = {
+      status: result.status,
+      generatedSql: result.generatedSQL,
+    };
+
+    switch (result.status) {
+      case 'completed':
+        return res.status(HttpStatus.OK).json({
+          ...response,
+          result: result.queryResult,
+        });
+
+      case 'pending':
+      case 'processing':
+        return res.status(HttpStatus.OK).json(response);
+
+      case 'error':
+        return res.status(HttpStatus.BAD_REQUEST).json({
+          ...response,
+          error: result.error,
+        });
+
+      default:
+        return res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .json({ message: 'Unknown task status' });
     }
   }
 }
